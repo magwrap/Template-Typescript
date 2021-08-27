c43d2507a0d8e5833994997d3f646e256bcd0dc9
Jan <jasiek.musiol@gmail.com>
2021-08-27 11:07:34 +0200

    doadnie info do projektu

---
+++ PracticeApp/info.ts
@@ -0,0 +1,17 @@
+// https://youtu.be/Hln37dE19bs - 21
+
+// npm install @react-navigation/native
+// expo install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/native-stack
+
+// packages :
+// 	"@react-navigation/bottom-tabs": "^6.0.5",
+//     	"@react-navigation/drawer": "^6.1.4",
+//     	"@react-navigation/native": "^6.0.2",
+//     	"@react-navigation/native-stack": "^6.1.0",
+//     	"@react-navigation/stack": "^6.0.7",
+
+// 	"react-native-gesture-handler": "^1.10.3",
+//    	"react-native-safe-area-context": "3.2.0",
+//   	"react-native-screens": "~3.4.0",
+//    	"react-native-web": "~0.13.12"
